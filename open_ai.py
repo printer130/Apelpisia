16afd839bbb6e60e3a99941c3bc77bf45979dad6
printer130 <leonardotobe94@gmail.com>
2022-12-20 17:17:27 -0400

    env

--- open_ai.py
+++ open_ai.py
@@ -4,10 +4,8 @@ import openai
 
 load_dotenv()
 model = "text-davinci-003"
-#openai.api_key = os.environ.get('OPENAI_KEY')
-#openai.organization = os.environ.get("OPENAI_ORG")
-openai.api_key = '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'
-openai.organization = 'org-GM55GnAr7nrRtA2p4TA8npK8'
+openai.api_key = os.environ.get('OPENAI_KEY')
+openai.organization = os.environ.get("OPENAI_ORG")
 
 def msg_from_openai(prompt_text):
   return openai.Completion.create(
