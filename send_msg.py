5a8564d3d1d62eca1550e65f83fd91af77d9dd61
Leonardo Torrico <53497625+printer130@users.noreply.github.com>
2022-12-20 21:19:56 -0400

    Update send_msg.py

--- send_msg.py
+++ send_msg.py
@@ -6,9 +6,6 @@ auth_token = os.environ['TWILIO_AUTH_TOKEN']
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 client = Client(account_sid, auth_token)
 
-twilio_default_number = "whatsapp:+14155238886"
-twilio_to = "whatsapp:+59174361042"
-
 def send_msg(msg_from_whatsapp):
   message = client.messages.create(
     body=msg_from_whatsapp,
